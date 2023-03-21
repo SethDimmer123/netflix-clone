956eeedad2a8ef6b1457ab98429c12f9afcc4d86
Seth DImmer <sethdimmer1@gmail.com>
2023-03-21 10:02:00 -0400

    initial commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,22 @@
 function Header() {
-  return <div>Header</div>
+  return <header>
+    <div className="flex items-center space-x-2 md">{/**phone stylings (min-width) */}
+    <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
+    </div>
+  </header>
 }
 
 export default Header
