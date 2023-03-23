c2e0230ad0c769c78a239669c5f6db0f5a73d789
Seth DImmer <sethdimmer1@gmail.com>
2023-03-23 12:42:19 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['image.tmdb.org'],
+    domains: ['image.tmdb.org','rb.gy'],
   },
 }
 // image with src error because i am using a next.js image component
 
 // it needs me to use either width or height of image but in this case i am using 
 // layout fill when using fill the outer parent should be position absolute or relative.
+
+// when my next.config.js file changes when i add something
+// i need to restart the server
