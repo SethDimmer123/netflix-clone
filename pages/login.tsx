76a743ed90d85a01675d3d314fc29193764ed098
Seth DImmer <sethdimmer1@gmail.com>
2023-03-24 12:23:32 -0400

    initial commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,14 @@
 import next from "next"
 import Head from "next/head"
-import Image from "next/legacy/image"
+import Image from "next/image"
+import { useState } from "react"
 
-function Login() {
+function Login() {//useState hook for logging user in
+  const [login, setLogin] = useState(false)
+
+
   return <div className="relative flex h-screen w-screen flex col bg-black md:items-center md:justify-center md:bg-transparent">
-      {/* really good for seo */}
+      {/* really good for seo (head tags) */}
     <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,9 +16,41 @@ function Login() {
       <Image
         src="https://rb.gy/p2hphi"
         layout="fill"
-        className="-z-10  !hidden opacity-60 sm:!inline"
-        objectFit="cover" alt=""
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
       />
+     <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+       md:max-w-md md:px-14">
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input type="email" placeholder="Email" className="input"/>
+          </label>
+          <label className="inline-block w-full">
+          <input type="password" placeholder="Password" className="input" />
+          </label>
+        </div>
+
+        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+          Sign In
+          </button>
+
+          <div className="text-[gray]">
+            New to Netflix?{' '}
+            <button type="submit" className="text-white hover:underline">
+              Sign Up now
+              </button>
+          </div>
+      </form>
+
+
+
     </div>
 }
 
