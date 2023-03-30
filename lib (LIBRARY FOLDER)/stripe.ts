2bb3942ed0bcb526dde3b65a1bddab2edc582532
Seth DImmer <sethdimmer1@gmail.com>
2023-03-30 19:46:25 -0400

    initial commit

--- lib (LIBRARY FOLDER)/stripe.ts
+++ lib (LIBRARY FOLDER)/stripe.ts
@@ -8,10 +8,10 @@ import {
   const payments = getStripePayments(app, {//accepts the instance of my app and my options which are from 2 collections from installing the stripe extension
     productsCollection:'plans',
     customersCollection:'customers'
-  })
+  })//these above allow me to retrieve the payments
 
-  const loadCheckout = async (priceId: string) => {
-    await createCheckoutSession(payments, {
+  const loadCheckout = async (priceId: string) => {// load checkout function asynchronus function 
+    await createCheckoutSession(payments, {//payments option and paarameters
         price:priceId,
         success_url: window.location.origin, //uses actual domain value instead of localhost:3000
         cancel_url: window.location.origin,
